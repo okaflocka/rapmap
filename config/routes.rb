c63c1dd2fdd6b6ef664423aaa307dfce34097ff4
okaflocka <oka.simian@gmail.com>
2022-10-04 21:06:34 +0900

    JQuery地図

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: "artists#index"
-  resources :artists 
+  resources :artists do
+    collection do
+      get 'state'
+    end
+  end
   get 'search', to: "artists#search"
 end
